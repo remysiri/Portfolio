b08820a66a42eb762d7fa9f9e079b586875441f8
remysiri <remysiri@student.arteveldehs.be>
2019-11-01 16:06:56 +0100

    Cleanup menu animations

--- src/app/components/header/Menu.jsx
+++ src/app/components/header/Menu.jsx
@@ -4,6 +4,7 @@ import React, { useState, useCallback, useRef } from 'react';
 Libraries
 */
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 /*
 Components
@@ -16,16 +17,31 @@ Styling
 import Icon from '../../assets/menu.svg';
 import Discard from '../../assets/discard.svg';
 
+const transition = {
+    duration: 0.5,
+    ease: [0.43, 0.13, 0.23, 0.96]
+  };
+
 const Menu = () => {
     const [ menuExpanded, setMenuExpanded] = useState(false);
+    const [ animateMenu, setAnimateMenu ] = useState({});
     const menuIcon = useRef();
-    const menuPosition = useRect(menuIcon);
 
     const expandMenu = useCallback(() => {
         if(menuExpanded === false) {
             setMenuExpanded(true);
+            setAnimateMenu({
+                width: "100%",
+                height: "100%",
+                transition
+            });
         } else {
             setMenuExpanded(false);
+            setAnimateMenu({
+                width: 0,
+                height: 0,
+                transition
+            });
         }
     });
 
@@ -39,7 +55,7 @@ const Menu = () => {
         <section className="menu__wrapper">
             <section className={ classNames }>
                 <img src={ Icon } alt="menu" onClick={ expandMenu } ref={ menuIcon }/>
-                <div id="cover" className="cover"></div>
+                <motion.div animate={ animateMenu } className="cover"></motion.div>
 
                 <div id="open__menu" className="open__menu">
                     <img src={ Discard } alt="menu" onClick={ expandMenu } ref={ menuIcon }/>
