7542542fa111001fe60dd76289d08eba42c09815
remysiri <remysiri@student.arteveldehs.be>
2019-11-07 17:04:29 +0100

    update public folder

--- src/app/layouts/main/Main.jsx
+++ src/app/layouts/main/Main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { AnimatePresence } from 'framer-motion';
 
 /*
@@ -30,23 +29,6 @@ import NotFoundPage from '../../pages/not-found-page';
 const Main = () => {
     return (
         <section>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>Rémy S. — Portfolio</title>
-                <meta name="description" content="Portfolio of Rémy Sirichantho, a web developer and graphic designer. Currently based in Belgium." />
-                <link rel="canonical" href="http://lymih.be" />
-                <meta name="robots" content="index, follow" />
-                <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
-                <meta property="og:title" content="Rémy S. — Portfolio" />
-                <meta property="og:site_name" content="Rémy S. — Portfolio" />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="http://lymih.be" />
-                <meta property="og:description" content="Portfolio of Rémy Sirichantho, a web developer and graphic designer. Currently based in Belgium." />
-                <meta name="twitter:site" content="http://lymih.be" />
-                <meta name="twitter:creator" content="@RemySirichantho" />
-                <meta name="twitter:title" content="Rémy S. — Portfolio" />
-                <meta name="twitter:description" content="Portfolio of Rémy Sirichantho, a web developer and graphic designer. Currently based in Belgium." />
-            </Helmet>
             <Header />
                 <AnimatePresence exitBeforeEnter>
                 <Switch>
