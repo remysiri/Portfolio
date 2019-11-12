38e6e119e01db1fe0ab8d5d722243993ac3168bd
remysiri <remysiri@student.arteveldehs.be>
2019-11-12 20:38:49 +0100

    fix carousel responsiveness & introtext

--- src/app/components/introduction/Intro.jsx
+++ src/app/components/introduction/Intro.jsx
@@ -10,8 +10,8 @@ const Intro = () => (
     <section className="introduction">
         <h1>
             <span className="hello">Hi, I </span>
-            <span className="speciality-one">Design <span>media</span></span>
-            <span className="speciality-two">Develop <span>cross-platform</span></span>
+            <span className="speciality-one">Design</span>
+            <span className="speciality-two">Develop <span className="bold">cross-platform</span></span>
         </h1>
     </section>
 );
